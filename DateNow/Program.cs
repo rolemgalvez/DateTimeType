754de37e13a44f442073cf2f83a02df53b69f2b3
Rolem Galvez <rolemgalvez@gmail.com>
2022-11-04 15:42:18 +0800

    Add project files.

---
+++ DateNow/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// Welcome message
+Console.WriteLine("Date Now");
+Console.WriteLine("--------");
+Console.WriteLine();
+
+// Declare variables
+DateTime dateNow = DateTime.Now;
+string day = dateNow.ToString("dd");
+string month = dateNow.ToString("MMMM");
+string year = dateNow.ToString("yyyy");
+
+// Show values
+Console.WriteLine($"I just want to share, it's day {day} of {month} {year}.");
+Console.WriteLine();
+Console.WriteLine("That's all. Bye!");
+Console.ReadLine();
